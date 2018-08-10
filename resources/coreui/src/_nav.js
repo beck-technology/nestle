ff3c9f8fb792f7fee3a93a51feb1773d1e9a1174
beck-technology <beck-technology@hotmail.com>
2018-08-10 11:45:18 +1000

    Fresh

---
+++ resources/coreui/src/_nav.js
@@ -0,0 +1,161 @@
+export default {
+	items: [{
+			name: 'Home',
+			url: '/dashboard',
+			icon: 'icon-speedometer',
+			children: [
+				{
+					name: 'My Dashboard',
+					url: '/dashboard',
+					// icon: 'icon-puzzle'
+				},
+			]
+		},
+		// {
+		// 	title: true,
+		// 	name: 'UI elements',
+		// 	class: '',
+		// 	wrapper: {
+		// 		element: '',
+		// 		attributes: {}
+		// 	}
+		// },
+		{
+			name: 'Foundation',
+			url: '/fundamentals',
+			icon: 'icon-star',
+			children: [
+				{
+					name: 'Role Type List',
+					url: '/roleTypes',
+					// icon: 'icon-puzzle'
+				},
+			]
+		},
+		{
+			name: 'Assets',
+			url: '/assets',
+			icon: 'icon-puzzle',
+			children: [
+				{
+					name: 'Asset List',
+					url: '/assets',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Asset Model List',
+					url: '/assetModels',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Asset Model Type List',
+					url: '/modelTypes',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Job List',
+					url: '/jobs',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Job Status List',
+					url: '/jobStats',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Job Type List',
+					url: '/jobTypes',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Job Priority List',
+					url: '/priorities',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Spare List',
+					url: '/spares',
+					// icon: 'icon-puzzle'
+				},
+				{
+					name: 'Spare Type List',
+					url: '/spareTypes',
+					// icon: 'icon-puzzle'
+				},
+			]
+		},
+		
+		// {
+		// 	name: 'Icons',
+		// 	url: '/icons',
+		// 	icon: 'icon-star',
+		// 	children: [{
+		// 			name: 'Font Awesome',
+		// 			url: '/icons/font-awesome',
+		// 			icon: 'icon-star',
+		// 			badge: {
+		// 				variant: 'secondary',
+		// 				text: '4.7'
+		// 			}
+		// 		},
+		// 		{
+		// 			name: 'Simple Line Icons',
+		// 			url: '/icons/simple-line-icons',
+		// 			icon: 'icon-star'
+		// 		}
+		// 	]
+		// },
+		// {
+		// 	name: 'Widgets',
+		// 	url: '/widgets',
+		// 	icon: 'icon-calculator',
+		// 	badge: {
+		// 		variant: 'primary',
+		// 		text: 'NEW'
+		// 	}
+		// },
+		// {
+		// 	name: 'Charts',
+		// 	url: '/charts',
+		// 	icon: 'icon-pie-chart'
+		// },
+		// {
+		// 	divider: true
+		// },
+		// {
+		// 	title: true,
+		// 	name: 'Extras'
+		// },
+		// {
+		// 	name: 'Pages',
+		// 	url: '/pages',
+		// 	icon: 'icon-star',
+		// 	children: [{
+		// 			name: 'Login',
+		// 			url: '/pages/login',
+		// 			icon: 'icon-star'
+		// 		},
+		// 		{
+		// 			name: 'Register',
+		// 			url: '/pages/register',
+		// 			icon: 'icon-star'
+		// 		},
+		// 		{
+		// 			name: 'Error 404',
+		// 			url: '/pages/404',
+		// 			icon: 'icon-star'
+		// 		},
+		// 		{
+		// 			name: 'Error 500',
+		// 			url: '/pages/500',
+		// 			icon: 'icon-star'
+		// 		}
+		// 	]
+		// },
+		// {
+		// 	name: 'Restaurants',
+		// 	url: '/restaurants',
+		// 	icon: 'icon-pie-chart'
+		// }
+	]
+}
